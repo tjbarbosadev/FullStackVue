ca1739591bcacd215e2e8e23eccf40e6b2053d51
Thiago Barbosa <tjbarbosa.dev@gmail.com>
2025-07-24 21:34:37 -0300

    Adjusting data for entities instead of db

--- src/main.ts
+++ src/main.ts
@@ -1,30 +1,52 @@
 import express from 'express';
 import pgp from 'pg-promise';
+import Board from './entity/Board';
+import Column from './entity/Column';
+import Card from './entity/Card';
+
 const app = express();
 const connection = pgp()(
   'postgres://postgres:admin123@localhost:5432/postgres'
 );
 
 app.get('/boards', async (req, res) => {
-  const boards = await connection.query('select * from vuefs.board', []);
+  const boardsData = await connection.query('select * from vuefs.board', []);
+  const boards: Board[] = [];
+
+  for (const boardData of boardsData) {
+    boards.push(new Board(boardData.name));
+  }
+
   console.log(boards);
   res.json(boards);
 });
 
 app.get('/boards/:idBoard/columns', async (req, res) => {
-  const columns = await connection.query(
+  const columnsData = await connection.query(
     'select * from vuefs.column where id_board = $1',
     [req.params.idBoard]
   );
+  const columns: Column[] = [];
+
+  for (const columnData of columnsData) {
+    columns.push(new Column(columnData.name, columnData.has_estimative));
+  }
+
   console.log(columns);
   res.json(columns);
 });
 
 app.get('/boards/:idBoard/columns/:idColumn/cards', async (req, res) => {
-  const cards = await connection.query(
+  const cardsData = await connection.query(
     'select * from vuefs.card where id_column = $1',
     [req.params.idColumn]
   );
+  const cards: Card[] = [];
+
+  for (const cardData of cardsData) {
+    cards.push(new Card(cardData.title, cardData.estimative));
+  }
+
   console.log(cards);
   res.json(cards);
 });
--- test/integration/API.test.ts
+++ test/integration/API.test.ts
@@ -21,6 +21,7 @@ test('Deve retornar as colunas de um quadro por meio da API', async () => {
   expect(columns).toHaveLength(3);
   const [column1, column2, column3] = columns;
   expect(column1.name).toBe('Coluna A');
+  expect(column1.hasEstimative).toBe(true);
   expect(column2.name).toBe('Coluna B');
   expect(column3.name).toBe('Coluna C');
 });
