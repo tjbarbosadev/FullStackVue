7fdc760dabcf8c077a9300a7e3a72a733c523982
Thiago Barbosa <tjbarbosa.dev@gmail.com>
2025-07-25 09:28:36 -0300

    Implementing custom http routes with express adapter and first controller

--- src/main.ts
+++ src/main.ts
@@ -1,36 +1,9 @@
-import express from 'express';
-import pgp from 'pg-promise';
-import ColumnService from './service/ColumnService';
-import BoardService from './service/BoardService';
-import CardService from './service/CardService';
-import BoardRepositoryDatabase from './infra/repository/BoardRepositoryDatabase';
+import BoardController from './infra/controller/BoardController';
 import PgPromiseConnection from './infra/database/PgPromiseConnection';
-import ColumnsRepositoryDatabase from './infra/repository/ColumnRepositoryDatabase';
-import CardRepositoryDatabase from './infra/repository/CardRepositoryDatabase';
-
-const app = express();
-
+import ExpressAdapter from './infra/http/ExpressAdapter';
 const connection = new PgPromiseConnection();
-const boardRepository = new BoardRepositoryDatabase(connection);
-const columnRepository = new ColumnsRepositoryDatabase(connection);
-const cardRepository = new CardRepositoryDatabase(connection);
 
-app.get('/boards', async (req, res) => {
-  const boardService = new BoardService(boardRepository);
-  const boards = await boardService.getBoards();
-  res.json(boards);
-});
+const http = new ExpressAdapter();
+new BoardController(http, connection);
 
-app.get('/boards/:idBoard/columns', async (req, res) => {
-  const columnService = new ColumnService(columnRepository);
-  const columns = await columnService.getColumns(parseInt(req.params.idBoard));
-  res.json(columns);
-});
-
-app.get('/boards/:idBoard/columns/:idColumn/cards', async (req, res) => {
-  const cardService = new CardService(cardRepository);
-  const cards = await cardService.getCards(parseInt(req.params.idColumn));
-  res.json(cards);
-});
-
-app.listen(3000);
+http.listen(3000);
